a73e75788694dc00100c5980ef33287244fb8055
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-01-23 17:42:03 -0600

    Positioning UI elements, input types

--- 4. UI_basics/app/src/main/java/com/example/ui_basics/MainActivity.java
+++ 4. UI_basics/app/src/main/java/com/example/ui_basics/MainActivity.java
@@ -3,15 +3,40 @@ package com.example.ui_basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button2:
+                Log.d("Message", "button2");
+                Toast.makeText(this, "button2", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                TextView text = findViewById(R.id.txtWelcome);
+//                text.setText("Hello World");
+//            }
+//        });
     }
 
     public void onHelloBtnCLicked(View view){
