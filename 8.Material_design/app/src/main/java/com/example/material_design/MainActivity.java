f490a429d6d315650dd2adc7074c625df26c6a5c
AndyFooGuoZhen <ndfucoding@gmail.com>
2022-01-25 10:30:36 -0600

    Importing MatrialCOmponents, floatingButton

---
+++ 8.Material_design/app/src/main/java/com/example/material_design/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.material_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+private FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
