b2c15c0ecc27fb7790a274defb699a99f9099fd8
Andy Foo <andyfoo@iastate.edu>
2022-02-03 10:42:40 -0600

    Get and Put from springboot server

--- 12.Retrofit/POST_Retrofit_Linking_Springboot/app/src/main/java/com/example/post_springboot/ApiInterface.java
+++ 12.Retrofit/POST_Retrofit_Linking_Springboot/app/src/main/java/com/example/post_springboot/ApiInterface.java
@@ -2,6 +2,7 @@ package com.example.post_springboot;
 
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.UUID;
 
 import retrofit2.Call;
@@ -9,7 +10,10 @@ import retrofit2.http.Body;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ApiInterface {
 
@@ -17,4 +21,11 @@ public interface ApiInterface {
     @POST("/api/v1/person")
     Call<Person> getUserInformation(@Body Person person);
 //    Call<Person> getUserInformation(@Body Person person);
+
+    @GET("/api/v1/person")
+    Call<List<Person>> retrieveUserInformation();
+
+    @PUT("/api/v1/person/{id}")
+    Call<Person> replacePerson(@Path("id") String id, @Body Person person);
+
 }
--- 12.Retrofit/POST_Retrofit_Linking_Springboot/app/src/main/java/com/example/post_springboot/MainActivity.java
+++ 12.Retrofit/POST_Retrofit_Linking_Springboot/app/src/main/java/com/example/post_springboot/MainActivity.java
@@ -12,6 +12,8 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,6 +21,8 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private Button postBtn;
+    private Button getBtn;
+    private Button putBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 postFunction();
+            }
+        });
+
+        getBtn = findViewById(R.id.getBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFunction();
+            }
+        });
+
+        putBtn = findViewById(R.id.putBtn);
+        putBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                putFunction();
             }
         });
     }
@@ -52,5 +72,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void getFunction(){
+        ApiInterface apiInterface = RetrofitClient.GetApiClientSeed().create(ApiInterface.class);
+        Call<List<Person>> call = apiInterface.retrieveUserInformation();
+
+        call.enqueue(new Callback<List<Person>>() {
+            @Override
+            public void onResponse(Call<List<Person>> call, Response<List<Person>> response) {
+
+                for(int i=0; i<response.body().size(); i++)
+                {
+                    Toast.makeText(MainActivity.this, response.body().get(i).getName(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Person>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void putFunction() {
+        ApiInterface apiInterface = RetrofitClient.GetApiClientSeed().create(ApiInterface.class);
+        Person person1 = new Person("Miami");
+
+        Call<Person> call = apiInterface.replacePerson("1d0fdcdd-a206-4b8c-bf5c-6d49d3b48ea1", person1);
+
+        call.enqueue(new Callback<Person>() {
+            @Override
+            public void onResponse(Call<Person> call, Response<Person> response) {
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Person> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
